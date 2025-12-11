1293769494adb5e7870e20ba9aa608dc0648543b
Mayank Verma <errmayank@gmail.com>
2025-12-12 00:19:12 +0530

    text: Initial TextBuffer implementation with rope

---
+++ crates/text/src/text.rs
@@ -0,0 +1,69 @@
+use ropey::Rope;
+use std::{
+    fmt::{self, Display, Formatter},
+    ops::Range,
+    str::FromStr,
+};
+
+#[derive(Clone, Debug)]
+pub struct TextBuffer {
+    rope: Rope,
+}
+
+impl TextBuffer {
+    pub fn new() -> Self {
+        Self { rope: Rope::new() }
+    }
+
+    pub fn len(&self) -> usize {
+        self.rope.len_bytes()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn insert(&mut self, offset: usize, text: &str) {
+        self.rope.insert(offset, text);
+    }
+
+    pub fn remove(&mut self, range: Range<usize>) {
+        self.rope.remove(range);
+    }
+
+    pub fn slice(&self, range: Range<usize>) -> String {
+        self.rope.slice(range).to_string()
+    }
+
+    pub fn byte_to_utf16(&self, byte_offset: usize) -> usize {
+        let char_offset = self.rope.byte_to_char(byte_offset);
+        self.rope.char_to_utf16_cu(char_offset)
+    }
+
+    pub fn utf16_to_byte(&self, utf16_offset: usize) -> usize {
+        let char_offset = self.rope.utf16_cu_to_char(utf16_offset);
+        self.rope.char_to_byte(char_offset)
+    }
+}
+
+impl Default for TextBuffer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl FromStr for TextBuffer {
+    type Err = anyhow::Error;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        Ok(Self {
+            rope: Rope::from_str(text),
+        })
+    }
+}
+
+impl Display for TextBuffer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.rope)
+    }
+}
